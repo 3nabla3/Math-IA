be1df28190443769f2abe4688f40fd30a6f9321b
Alban Guyon <albanguyon@yahoo.fr>
2020-09-20 21:44:30 -0400

    finished basic functionality

--- main.py
+++ main.py
@@ -2,18 +2,21 @@
 
 import random
 
+
+# Card with a suit and rank
 class Card:
     suits = ["heart", "club", "spade", "diamond"]
     values = [str(v) for v in range(2, 11)] + ["jack", "queen", "king", "ace"]
 
-    def __init__(self, suit, value):
+    def __init__(self, suit, rank):
         self.suit = suit
-        self.value = value
+        self.rank = rank
 
     def __str__(self):
-        return f"{self.value.title()} of {self.suit}s"
+        return f"{self.rank.title()} of {self.suit}s"
 
 
+# List of 52 playing cards each represented once
 class Deck:
     def __init__(self):
         self.cards = [
@@ -37,40 +40,39 @@ class Deck:
         return self.cards.pop()
 
 
-def print_cards(cards):
-    for card in cards:
-        print(card)
-
-
-class Player:
-    def __init__(self, name, card1, card2):
-        self.name  = name
-        self.card1 = card1
-        self.card2 = card2
+# List of 5 cards that can be checked for combinations
+class Combo:
+    def __init__(self, cards):
+        assert len(cards) == 5
+        self.cards = cards
 
     def __str__(self):
-        result = f"{self.name}'s cards:\n"
-        result += "-------------------\n"
-        result += str(self.card1) + "\n"
-        result += str(self.card2) + "\n"
+        result = ""
+        for card in self.cards:
+            result += str(card) + "\n"
         return result
+
+    def check():
+        pass
+
+
+def print_cards(cards):
+    for card in cards:
+            print(card)
 
 
 def main():
-    num_players = 5
-    deck = Deck()
-    deck.shuffle()
+    # Create the deck
+    d = Deck()
+    d.shuffle()
 
-    common_cards = [deck.deal() for _ in range(5)]
-    print("Common cards:")
-    print("-------------------")
-    print_cards(common_cards)
-    print()
-    print()
+    # deal 5 random cards
+    current_cards = []
+    for i in range(5):
+        current_cards.append(d.deal())
 
-    players = [Player(f"Player {i}",deck.deal(), deck.deal())for i in range(num_players)]
-    for player in players:
-        print(player)
+    c = Combo(current_cards)
+    print(c)
 
 if __name__ == "__main__":
     main()
