b581dac90c855072112c3d59df2c8fd83e27c41b
Alban Guyon <albanguyon@yahoo.fr>
2020-09-25 14:27:37 -0400

    removed print statements

--- main.py
+++ main.py
@@ -6,13 +6,6 @@ from multiprocessing import Process, Value, Manager
 import os
 import sys
 
-# print thought process
-debug = False
-
-def print_if_debug(string=None):
-    if debug:
-        print(string)
-
 # Card with a suit and rank
 class Card:
     suits = ["heart", "club", "spade", "diamond"]
@@ -50,66 +43,43 @@ class Combo:
         return result
 
     def same_suit(self):
-        print_if_debug("Are all of the cards the same suit?")
         for card in self.cards:
             if not self.cards[0].suit == card.suit:
-                print_if_debug("no")
                 return None
-        print_if_debug("yes")
         return self.cards[0].suit
 
     def in_order(self):
         status = None
         o = sorted(self.cards)
-        print_if_debug("Are the cards in order?")
         in_order = True
         for i, c in enumerate(self.cards[:-1]):
             if not c.val() + 1 == self.cards[i + 1].val():
-                print_if_debug("no")
                 return status
-
-        print_if_debug("yes")
         status = self.cards[-1].val()
-
         return status
 
     def has_pair(self):
         status = []
         list_of_ranks = [c.val() for c in self.cards]
-        print_if_debug("Is there a pair?")
         for r in Card.values:
             if list_of_ranks.count(r) == 2:
-                print_if_debug("yes")
                 status.append(r)
-        if status == None:
-             print_if_debug("no")
-
         return status
 
     def has_triple(self):
         status = []
         list_of_ranks = [c.val() for c in self.cards]
-        print_if_debug("Is there a three of a kind?")
         for r in Card.values:
             if list_of_ranks.count(r) == 3:
-                print_if_debug("yes")
                 status.append(r)
-        if status == None:
-            print_if_debug("no")
-
         return status
 
     def has_quad(self):
         status = []
         list_of_ranks = [c.val() for c in self.cards]
-        print_if_debug("Are there four of a kind?")
         for r in Card.values:
             if list_of_ranks.count(r) == 4:
-                print_if_debug("yes")
                 status.append(r)
-        if status == None:
-            print_if_debug("no")
-
         return status
 
     def check(self):
@@ -117,12 +87,10 @@ class Combo:
         temp1 = self.same_suit()
         temp2 = self.in_order()
         if temp1 and temp2:
-            print_if_debug("Is the highest card an ace?")
+            # is the highest card an ace
             if temp2 == 14:
-                print_if_debug("yes")
                 return ["RF", [temp1,]]
             else:
-                print_if_debug("no")
                 return ["SF", [temp1, temp2]]
 
         # is it a four of a kind?
@@ -156,12 +124,7 @@ class Combo:
 
         # base condition
         temp = sorted([c.val() for c in self.cards])
-        print_if_debug(temp)
         return ["HC", [temp[-1],]]
-
-# def print_cards(cards):
-#     for card in cards:
-#             print(card)
 
 
 def worker(keep_going, return_dict, procnum, gcount, target):
@@ -172,33 +135,11 @@ def worker(keep_going, return_dict, procnum, gcount, target):
         c = None
         result = None
         while keep_going.value:
-            # Create the deck
-            # d = Deck()
-            # d.shuffle()
-
             # deal 5 random cards
-            # current_cards = [d.deal() for i in range(5)]
             current_cards = [Card(random.choice(Card.suits), random.choice(Card.ranks)) for i in range(5)]
-            # current_cards = [
-            #     Card("diamond", "3"),
-            #     Card("diamond", "4"),
-            #     Card("diamond", "5"),
-            #     Card("diamond", "6"),
-            #     Card("diamond", "7")
-            # ]
 
-            print_if_debug("Current cards:")
-            print_if_debug("------------------------------")
             c = Combo(current_cards)
-            print_if_debug(c)
-            print_if_debug()
-            print_if_debug("Thought process:")
-            print_if_debug("------------------------------")
             result = c.check()
-            print_if_debug()
-            print_if_debug("Result:")
-            print_if_debug("------------------------------")
-            print_if_debug(result)
             if result[0] == target:
                 keep_going.value = False
                 return_dict[procnum] = [c, result]
@@ -220,8 +161,6 @@ def main():
     for i in p:
         i.start()
 
-    # time.sleep(2)
-    # keep_going.value = False
     try:
         for i in p:
             i.join()
