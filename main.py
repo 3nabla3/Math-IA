22f2c97d209c99b612cff777d45b57f880d17f99
Alban Guyon <albanguyon@yahoo.fr>
2020-09-27 10:39:33 -0400

    created freq chart

--- main.py
+++ main.py
@@ -89,91 +89,110 @@ class Combo:
         if temp1 and temp2:
             # is the highest card an ace
             if temp2 == 14:
-                return ["RF", [temp1,]]
+                return "RF"
             else:
-                return ["SF", [temp1, temp2]]
+                return "SF"
 
         # is it a four of a kind?
         temp = self.has_quad()
-        if temp: return ["FoaK", temp]
+        if temp: return "FoaK"
 
         # is it a full house?
         temp1 = self.has_triple()
         temp2 = self.has_pair()
-        if temp1 and temp2: return ["FH", temp1 + temp2]
+        if temp1 and temp2: return "FH"
 
         # is it a flush?
         temp = self.same_suit()
-        if temp: return ["Flush", temp]
+        if temp: return "F"
 
         # is it a straight
         temp = self.in_order()
-        if temp: return ["Straight", temp]
+        if temp: return "S"
 
         # is it a Three of a kind
         temp = self.has_triple()
-        if temp: return ["ToaK", temp]
+        if temp: return "ToaK"
 
         # is it a two pair
         temp = self.has_pair()
-        if len(temp) == 2: return ["TP", temp]
+        if len(temp) == 2: return "TP"
 
         # is it a pair
         temp = self.has_pair()
-        if temp: return ["P", temp]
+        if temp: return "P"
 
         # base condition
         temp = sorted([c.val() for c in self.cards])
-        return ["HC", [temp[-1],]]
+        return "HC"
 
 
-def worker(keep_going, return_dict, procnum, gcount, target):
+def worker(keep_going, return_dict, procnum):
     try:
-        count = 0
-        t1 = time.time()
+        combo_name = None
         running = True
         c = None
-        result = None
+        # create a temp dict of combo freq
+        amount_per_combo = {"HC": 0, "P": 0, "TP" : 0, "ToaK": 0, "S": 0, "F": 0, "FH": 0, "FoaK": 0, "SF": 0, "RF": 0}
         while keep_going.value:
             # deal 5 random cards
             current_cards = [Card(random.choice(Card.suits), random.choice(Card.ranks)) for i in range(5)]
-
             c = Combo(current_cards)
-            result = c.check()
-            if result[0] == target:
-                keep_going.value = False
-                return_dict[procnum] = [c, result]
-            count += 1
 
-        gcount.value += count
+            # check the 5 cards for combo
+            combo_name = c.check()
+            amount_per_combo[combo_name] += 1
     except(KeyboardInterrupt):
         pass
+    finally:
+        return_dict[procnum] = amount_per_combo
 
 
 
 def main():
+    # set global values
     keep_going = Value('b', True)
-    count = Value('i', 0)
     return_dict = Manager().dict()
-    target = sys.argv[1]
-    assert target in ["HC", "P", "TP", "ToaK", "Straight", "Flush", "FH", "FoaK", "SF", "RF"]
-    p = [Process(target=worker, args=(keep_going, return_dict, i, count, target)) for i in range(os.cpu_count())]
+
+    # search time 10s by default
+    search_time = 10
+    if len(sys.argv) > 1:
+        search_time = float(sys.argv[1])
+    if search_time <= 0:
+        raise ValueError("search time must be a positive value")
+
+    # create processes and start them
+    p = [Process(target=worker, args=(keep_going, return_dict, i)) for i in range(os.cpu_count())]
+    start_time = time.time()
     for i in p:
         i.start()
 
+
+    # stop all processes after the seach time and wait for them
     try:
+        time.sleep(search_time)
+        keep_going.value = False
+    # if the program is quit, display what ever is already found
+    except(KeyboardInterrupt):
+        keep_going.value = False
+        print()
+    finally:
         for i in p:
             i.join()
-    except(KeyboardInterrupt):
-        pass
 
-    for combo, result in return_dict.values():
-        print(combo)
-        print(result)
-        print()
-        print()
+    # create a global dict with freq of each combo
+    g_amount_per_combo = {"HC": 0, "P": 0, "TP" : 0, "ToaK": 0, "S": 0, "F": 0, "FH": 0, "FoaK": 0, "SF": 0, "RF": 0}
 
-    print(f"Took {count.value} tries")
+    # populate that dict with the worker dict
+    for procnum in return_dict:
+        for combo_name in return_dict[procnum]:
+            g_amount_per_combo[combo_name] += return_dict[procnum][combo_name]
+
+    # display the freq of each combo
+    for combo_name in g_amount_per_combo:
+        print(f"{combo_name}: \t {g_amount_per_combo[combo_name]}")
+
+    print(f"Took {sum(g_amount_per_combo.values())} tries and {time.time() - start_time} seconds")
 
 
 if __name__ == "__main__":
